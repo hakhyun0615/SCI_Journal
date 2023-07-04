42ab00bfa7caec73fa3fa61faaa34d4900403c08
Hwankyu Yoo <hkyoo52hkyoo@gmail.com>
2023-07-04 22:45:54 +0900

    train 수정, econmy.py 추가

---
+++ 코드/Dataset/economy.py
@@ -0,0 +1,20 @@
+import torch
+from torch.utils.data import Dataset
+
+class EconomyDataset(Dataset):
+      def __init__(self, data, step = 5):
+            X,Y = [],[]
+            for i in range(len(data)-step-1):
+                  X.append(data[i:i+step])
+                  Y.append(data[i+step])
+
+            self.x = torch.FloatTensor(X)
+            self.y = torch.FloatTensor(Y)
+
+            self.len = len(X)
+
+      def __getitem__(self, i):
+            return self.x[i], self.y[i]
+
+      def __len__(self):
+            return self.len
