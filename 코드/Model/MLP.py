78b94c8d97d2b269aa611e975d6db129fa38b6b5
KimHakHyun <hakhyun0615@naver.com>
2023-07-10 14:15:40 +0900

    main

--- 코드/Model/MLP.py
+++ 코드/Model/MLP.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_dim, hidden_dim, output_dim):
         super(MLP, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
+            nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_size, output_size)
+            nn.Linear(hidden_dim, output_dim)
         )
 
     def forward_once(self, x):
