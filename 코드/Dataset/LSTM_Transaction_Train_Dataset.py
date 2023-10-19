ddc2c396fc32ae561c388f20ce96cbe09e1e7eea
hakhyun0615 <hakhyun0615@naver.com>
2023-10-20 00:43:36 +0900

    main

--- 코드/Dataset/LSTM_Transaction_Train_Dataset.py
+++ 코드/Dataset/LSTM_Transaction_Train_Dataset.py
@@ -13,17 +13,16 @@ class LSTM_Transaction_Train_Dataset(Dataset):
             for apartment_complex in interpolated_df[interpolated_df['동'] == dong]['단지'].unique():
                 filtered_interpolated_df = interpolated_df[(interpolated_df['동'] == dong)*(interpolated_df['단지'] == apartment_complex)]
                 filtered_interpolated_df_values = filtered_interpolated_df['제곱미터당 거래금액'].values
-                filtered_interpolated_df_times = filtered_interpolated_df['계약년월'].apply(lambda x: float((x.year-pd.Timestamp('2006-01').year)*12+(x.month-pd.Timestamp('2006-01').month)+1)).values
                 for idx in range(len(filtered_interpolated_df_values)-window_size):
-                    dongs_x.append([filtered_interpolated_df_values[idx:idx+window_size],filtered_interpolated_df_times[idx:idx+window_size]])
-                    dongs_y.append([filtered_interpolated_df_values[idx+window_size:idx+window_size+1],filtered_interpolated_df_times[idx+window_size:idx+window_size+1]])
+                    dongs_x.append(filtered_interpolated_df_values[idx:idx+window_size])
+                    dongs_y.append(filtered_interpolated_df_values[idx+window_size:idx+window_size+1])
         self.dongs_x = dongs_x
         self.dongs_y = dongs_y
         self.len = len(dongs_x)
 
-    # 부동산_x, 부동산_시점_x, 부동산_y, 부동산_시점_y 
+    # 부동산_x, 부동산_y
     def __getitem__(self, i):
-        return torch.FloatTensor(self.dongs_x[i][0]), torch.FloatTensor(self.dongs_x[i][1]), torch.FloatTensor(self.dongs_y[i][0]), torch.FloatTensor(self.dongs_y[i][1])
+        return torch.FloatTensor(self.dongs_x[i]), torch.FloatTensor(self.dongs_y[i])
 
     def __len__(self):
         return self.len
