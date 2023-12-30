d390746f535eebb00f665d7b94bebf3ada05d5c7
KimHakHyun <hakhyun0615@naver.com>
2023-12-31 02:21:14 +0900

    main

--- 코드/Model/Embedding.py
+++ 코드/Model/Embedding.py
@@ -1,21 +1,29 @@
 from torch import nn
 
 class Embedding(nn.Module):
-    def __init__(self, encoder_dim_1, encoder_dim_2, embedding_dim, decoder_1, decoder_2):
+    def __init__(self, encoder_dim_1, encoder_dim_2, encoder_dim_3, embedding_dim, decoder_1, decoder_2, decoder_3):
         super(Embedding, self).__init__()
 
-        self.encoder_1 = nn.Linear(12, encoder_dim_1)
-        self.encoder_2 = nn.Linear(encoder_dim_1, encoder_dim_2)
-        self.embedding = nn.Linear(encoder_dim_2, embedding_dim)
-        self.decoder_1 = nn.Linear(embedding_dim, decoder_1)
-        self.decoder_2 = nn.Linear(decoder_1, decoder_2)
-        self.output = nn.Linear(decoder_2, 1)
+        self.encoder = nn.Sequential(
+            nn.Linear(12, encoder_dim_1),
+            nn.ReLU(True),
+            nn.Linear(encoder_dim_1, encoder_dim_2),
+            nn.ReLU(True),
+            nn.Linear(encoder_dim_2, encoder_dim_3),
+            nn.ReLU(True),
+            nn.Linear(encoder_dim_3, embedding_dim),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(embedding_dim, decoder_1),
+            nn.ReLU(True),
+            nn.Linear(decoder_1, decoder_2),
+            nn.ReLU(True),
+            nn.Linear(decoder_2, decoder_3),
+            nn.ReLU(True),
+            nn.Linear(decoder_3, 1),
+        )
 
     def forward(self, x):
-        x = self.encoder_1(x)
-        x = self.encoder_2(x)
-        x = self.embedding(x)
-        x = self.decoder_1(x)
-        x = self.decoder_2(x)
-
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
