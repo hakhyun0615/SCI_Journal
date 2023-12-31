c05ac9ca4d5e555f5f1a5db446785b2a48bee181
KimHakHyun <hakhyun0615@naver.com>
2023-12-31 17:18:18 +0900

    main

--- 코드/Model/NLinear.py
+++ 코드/Model/NLinear.py
@@ -1,30 +1,30 @@
-import torch
 import torch.nn as nn
+import torch
 
 class NLinear(nn.Module):
     def __init__(self, emb_dim, window_size):
         super(NLinear, self).__init__()
         self.emb_dim = emb_dim
         self.window_size = window_size
-
-        self.Linear = nn.ModuleList([nn.Linear(window_size, 1) for _ in range(emb_dim)])
-        self.BatchNorm = nn.ModuleList([nn.BatchNorm1d(1) for _ in range(emb_dim)])
-        self.Linear2 = nn.Linear(emb_dim, 1)
-
+        self.Linear = torch.nn.ModuleList()
+        self.BatchNorm = torch.nn.ModuleList()
+        for i in range(self.emb_dim):
+            self.Linear.append(nn.Linear(self.window_size, 1))
+            self.BatchNorm.append(nn.BatchNorm1d(1))
+        self.Linear2 = nn.Linear(self.emb_dim, 1)
+    
     def forward(self, x):
-        seq_last = x[:, -1, :].detach()
-
+        seq_last = x[:,-1,:].detach()  # num * 1 * emb_dim
         for i in range(len(x)):
-            x[:, i, :] -= seq_last 
+            x[:,i,:] = x[:,i,:] - seq_last 
 
         output = torch.zeros([x.size(0), self.emb_dim], dtype=x.dtype).to(x.device)
-
         for i in range(self.emb_dim):
-            linear_out = self.Linear[i](x[:, :, i])
-            batch_normalized_out = self.BatchNorm[i](linear_out)
-            output[:, i] = batch_normalized_out.squeeze()
+            linear_output = self.Linear[i](x[:,:,i])
+            bn_output = self.BatchNorm[i](linear_output)
+            output[:,i] = bn_output.squeeze()
 
-        output += seq_last.squeeze()
-        output = self.Linear2(output)
-
+        output += seq_last.squeeze()  # num * emb_dim  
+        output = self.Linear2(output)  # num * 1
+        
         return output
